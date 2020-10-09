84f2917869162a86198dcd48492967fabba0e326
arata <arata04962@gmail.com>
2020-10-09 18:21:26 +0900

    :wrench: add type-hinting

--- typetalk/client.py
+++ typetalk/client.py
@@ -6,17 +6,17 @@ BASE_URL = 'https://typetalk.com/api/v1'
 
 
 class Client(object):
-    def __init__(self, token):
+    def __init__(self, token: str) -> None:
         self.token = token
 
-    def getTopic(self, topic_id):
+    def getTopic(self, topic_id: int) -> topics.TopicDetails:
         url = f'{BASE_URL}/topics/{topic_id}'
         res_json = self._get(url)
         res = json.loads(res_json)
         topic_detail = topics.TopicDetails(res)
         return topic_detail
 
-    def _get(self, url):
+    def _get(self, url: str) -> str:
         headers = {'X-Typetalk-Token': self.token}
         req = request.Request(url, headers=headers)
         res = request.urlopen(req)
